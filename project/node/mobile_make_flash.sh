e427631b8a90193da76bdfc2688da4d7e28c05ed
Haoxi Tan <haoxi.tan@gmail.com>
2022-05-25 22:34:46 +1000

    mobile id can now be passed from command line

--- project/node/mobile_make_flash.sh
+++ project/node/mobile_make_flash.sh
@@ -1,2 +1,8 @@
+if [[ $# -eq 1 ]] ; then
+  echo "building with Mobile ID $1"
+  sed -i "s/M_ID=./M_ID=$1/" CMakeLists.txt
+else
+  echo "building with Mobile ID 1"
+  sed -i "s/M_ID=./M_ID=1/" CMakeLists.txt
+fi
 west build -b thingy52_nrf52832 -p auto -- -DMOBILE_NODE=ON && west flash -r jlink
-
