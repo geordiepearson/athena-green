6303620b74db818378e86e5d789bbd44fdf28e74
Haoxi Tan <haoxi.tan@gmail.com>
2022-05-27 17:56:16 +1000

    default static id of 5

--- project/node/static_make_flash.sh
+++ project/node/static_make_flash.sh
@@ -2,7 +2,7 @@ if [[ $# -eq 1 ]] ; then
   echo "building with Mobile ID $1"
   sed -i "s/M_ID=./M_ID=$1/" CMakeLists.txt
 else
-  echo "building with Mobile ID 1"
+  echo "building with Mobile ID 5"
   sed -i "s/M_ID=./M_ID=5/" CMakeLists.txt
 fi
 
