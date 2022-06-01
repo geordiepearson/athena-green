327355a966df9f1c9dcf153affabd03c17337415
Haoxi Tan <haoxi.tan@gmail.com>
2022-06-01 19:59:23 +1000

    xenon flash

---
+++ project/node/xenon_static_make_flash.sh
@@ -0,0 +1,10 @@
+if [[ $# -eq 1 ]] ; then
+  echo "building with Mobile ID $1"
+  sed -i "s/M_ID=./M_ID=$1/" CMakeLists.txt
+else
+  echo "building with Mobile ID 5"
+  sed -i "s/M_ID=./M_ID=5/" CMakeLists.txt
+fi
+
+west build -p auto -b particle_xenon  && west flash -r jlink
+
